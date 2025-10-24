5f37e52d35aad8faf12bfbbbebe09239d48a52c9
Leonardo Galli <postmaster@leodev.it>
2025-10-24 18:46:46 +0200

    Update 74181.c

--- 74181.c
+++ 74181.c
@@ -73,7 +73,7 @@
 #include <io.h>
 #include <process.h>
 #include <windows.h>
-#define sleep(x) Sleep(1000 * (x))  // Sleep usa millisecondi
+#define sleep(x) Sleep(1000 * (x))
 #else
 #include <unistd.h>
 #endif
