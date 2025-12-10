648ffa90b79ade0872eb8539e31261464afc74b6
Leonardo Galli <postmaster@leodev.it>
2025-12-10 08:19:55 +0100

    Update benchmark.py

--- .github/workflows/benchmark.py
+++ .github/workflows/benchmark.py
@@ -1,7 +1,7 @@
 import sys, os, subprocess, psutil, time, json
 from anybadge import Badge
 
-# --- Gestione argomento ---
+# --- Argomenti ---
 arg = sys.argv[1]
 os_name = sys.argv[2]
 
@@ -11,85 +11,97 @@ if "..." in arg:
 else:
     test_ids = [int(arg)]
 
-is_win = os.name == 'nt'
-exe = "simulator.exe" if is_win else "./simulator"
+exe = "simulator.exe" if os.name == "nt" else "./simulator"
 
-# --- Generazione input (uguale a prima) ---
+# --- Generazione input ottimizzata ---
 inputs = []
-for M in [0, 1]:
-    for i in range(16):
-        S3 = (i >> 3) & 1
-        S2 = (i >> 2) & 1
-        S1 = (i >> 1) & 1
-        S0 = i & 1
-        inp = "1\nN\n" + f"{S3}\n{S2}\n{S1}\n{S0}\n{M}\n0\n0\n0\n0\n0\n0\n0\n0\n"
-        inputs.append(inp)
 
-for M in [0, 1]:
-    for i in range(16):
-        S3 = (i >> 3) & 1
-        S2 = (i >> 2) & 1
-        S1 = (i >> 1) & 1
-        S0 = i & 1
-        inp = "6\nN\n4294967295\n4294967295\n" + f"{S3}\n{S2}\n{S1}\n{S0}\n{M}\n"
-        inputs.append(inp)
+# Funzione helper per generare segnali S3..S0
+def bits4(i):
+    return [(i >> b) & 1 for b in (3, 2, 1, 0)]
 
-edge4 = [
-    "1\nN\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n",
-    "1\nN\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n",
+# Test ALU 4 bit
+for M in (0, 1):
+    for i in range(16):
+        S3, S2, S1, S0 = bits4(i)
+        inputs.append(
+            f"1\nN\n{S3}\n{S2}\n{S1}\n{S0}\n{M}\n" + "0\n" * 8
+        )
+
+# Test ALU 32 bit
+for M in (0, 1):
+    for i in range(16):
+        S3, S2, S1, S0 = bits4(i)
+        inputs.append(
+            f"6\nN\n4294967295\n4294967295\n{S3}\n{S2}\n{S1}\n{S0}\n{M}\n"
+        )
+
+# Edge cases 4 bit
+inputs.extend([
+    "1\nN\n" + "0\n" * 12,
+    "1\nN\n" + "1\n" * 12,
     "1\nN\n0\n0\n0\n1\n0\n0\n1\n1\n0\n0\n1\n1\n",
-]
-inputs.extend(edge4)
+])
 
-edge32 = [
+# Edge cases 32 bit
+inputs.extend([
     "6\nN\n0\n0\n0\n0\n0\n0\n",
     "6\nN\n4294967295\n0\n0\n0\n0\n0\n",
     "6\nN\n0\n4294967295\n0\n0\n0\n0\n",
     "6\nN\n2147483647\n2147483647\n0\n0\n1\n0\n0\n",
-]
-inputs.extend(edge32)
+])
 
+# Espressioni
 exprs = [
     "2+2", "10-5", "3*7", "15/3", "(2+3)*4", "2**10", "100%7", "1+2+3+4+5",
     "1000000+2000000", "0/1", "1<<5", "0xFF+1", "2147483647+1", "-5+10",
     "(10*(2+3))-7", "((2+2)*2)+2", "100/0", "2+2*2", "1+1", "999999999"
 ]
-for e in exprs:
-    inputs.append(f"5\n{e}\n6\n")
+inputs.extend([f"5\n{e}\n6\n" for e in exprs])
 
+# Ultimi due test
 inputs.extend(["4\nN\n", "5\nN\n"])
 
-# --- Accumulatori globali ---
+# --- Benchmark super ottimizzato ---
 cpu_all, ram_all, time_all = [], [], []
 
-for test_id in test_ids:
-    if test_id >= len(inputs):
+for tid in test_ids:
+    if tid >= len(inputs):
         continue
 
-    stdin_data = inputs[test_id]
-    cpu_vals, ram_vals = [], []
+    stdin_data = inputs[tid]
 
-    proc = psutil.Popen([exe], stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    # Avvio processo
+    proc = psutil.Popen(
+        [exe],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+
     proc.stdin.write(stdin_data.encode())
     proc.stdin.close()
 
-    start = time.time()
-    while proc.poll() is None:
-        try:
-            cpu_vals.append(proc.cpu_percent(interval=0.001))
-            ram_vals.append(proc.memory_info().rss / (1024 * 1024))
-        except:
-            break
-        time.sleep(0.01)
-    duration = time.time() - start
+    # Misurazione veloce: 1 sola lettura CPU/RAM
+    start = time.perf_counter()
 
-    if cpu_vals:
-        cpu_all.append(sum(cpu_vals) / len(cpu_vals))
-    if ram_vals:
-        ram_all.append(sum(ram_vals) / len(ram_vals))
-    time_all.append(duration * 1000)
+    # Attesa processo SENZA polling lento
+    proc.wait()
 
-# --- Calcolo medie globali ---
+    duration = (time.perf_counter() - start) * 1000  # ms
+
+    try:
+        cpu = proc.cpu_percent()  # una sola lettura
+        ram = proc.memory_info().rss / (1024 * 1024)
+    except:
+        cpu = 0
+        ram = 0
+
+    cpu_all.append(cpu)
+    ram_all.append(ram)
+    time_all.append(duration)
+
+# --- Medie ---
 cpu_avg = sum(cpu_all) / len(cpu_all) if cpu_all else 0
 ram_avg = sum(ram_all) / len(ram_all) if ram_all else 0
 time_avg = sum(time_all) / len(time_all) if time_all else 0
